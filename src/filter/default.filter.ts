64284bc9d96cdcc2ea601f153c4011a1c16c237b
不动声色 <1609059419@qq.com>
2023-05-07 22:38:21 +0800

    feat:😝 add commits

--- src/filter/default.filter.ts
+++ src/filter/default.filter.ts
@@ -1,10 +1,10 @@
 import { Catch } from '@midwayjs/core';
 import { Context } from '@midwayjs/koa';
 
+// capture all uncalssified errors
 @Catch()
 export class DefaultErrorFilter {
   async catch(err: Error, ctx: Context) {
-    // 所有的未分类错误会到这里
     return {
       success: false,
       message: err.message,
