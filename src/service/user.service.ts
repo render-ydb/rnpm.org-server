3a0c953b134be3a68df4b48fd2e307f595fecd78
不动声色 <1609059419@qq.com>
2023-05-08 23:02:08 +0800

    feat: 🚀 defining the basic structure user servie

--- src/service/user.service.ts
+++ src/service/user.service.ts
@@ -1,14 +1,36 @@
 import { Provide } from '@midwayjs/core';
-import { IUserOptions } from '../interface';
+// import { IUserOptions } from '../interface';
 
 @Provide()
 export class UserService {
-  async getUser(options: IUserOptions) {
-    return {
-      uid: options.uid,
-      username: 'mockedName',
-      phone: '12345678901',
-      email: 'xxx.xxx@xxx.com',
-    };
+
+  async auth(userName: string, password: string) {
+    // TODO
+  }
+  async get(userName: string) {
+    // TODO
+  }
+
+  async list(users: Array<string>) {
+    // TODO
+  }
+  async search(query: string, options: Record<string, any>) {
+    // TODO
+  }
+
+  async getAndSave(userName: string) {
+    // TODO
+  }
+
+  async authAndSave(userName: string, password: string) {
+    // TODO
+  }
+
+  async add(user: Record<string, any>) {
+    // TODO
+  }
+
+  async update(user: Record<string, any>) {
+    // TODO
   }
 }
